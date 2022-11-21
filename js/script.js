5a8d4ae5a5bde2504549c08c6d670a258f73d5c9
Ridwan Alawode <ridwanalawode7@email.com>
2022-11-21 07:30:54 +0100

    Service Worker and Web App Manifest Added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 //DEFINE VARIABLES
-let NUMBER_OF_TRIALS
+let NUMBER_OF_TRIALS;
 let LEVEL, SPECIAL_NUMBER, NUMBER_OF_TRIALS_LEFT, MAXIMUM_NUMBER, FONT_SIZE;
-let message = ''
+let message = '';
 let hint = true;
 let action;
-console.log(document.querySelector('.test-text'));
-
-
 
 //SELECTED ELEMENTS
 let startButton = document.querySelector('.start');
@@ -15,248 +12,295 @@ let closeButton = document.querySelectorAll('.close-button');
 let rulesButton = document.querySelector('.rules-button');
 let creditsButton = document.querySelector('.credits-button');
 let welcomePage = document.querySelector('.welcome-page');
-let guessButton = document.querySelector('.btn-guess');
+let guessButton = document.querySelectorAll('.btn-guess');
 let restartButton = document.querySelector('.restart');
 let selectLevel = document.querySelector('.select-level');
 let yesButton = document.querySelector('.yes');
 let noButton = document.querySelector('.no');
 let playAgainButton = document.querySelector('.play-again');
-let selectDifficultyButton = document.querySelector('.select-difficulty'); 
+let selectDifficultyButton = document.querySelector('.select-difficulty');
 let keys = document.querySelector('.keys');
 let delButton = document.querySelector('.btn-del');
 let clearButton = document.querySelector('.btn-clr');
+var toggleBtn = document.querySelector('.toggle-button');
 
-document.querySelector('.toggle-button').addEventListener('click', function () {
-    if (hint == true) {
-        document.querySelector('.toggle-button').classList.remove('on')
-        hint = false;
-    } else if (hint == false) {
-        document.querySelector('.toggle-button').classList.add('on')
-        hint = true;
-    }
-})
+// MEDIA QUERY
+let mql = window.matchMedia('(max-width: 960px)');
 
-keys.addEventListener('click', function (e) {
-    
-    let clickedNumber = e.target.textContent;
-    if (!isNaN(clickedNumber)) {
-        document.querySelector('.input-number-field').textContent += clickedNumber;
-        document.querySelector('.input-field div').style.border = 'solid 5px #808080';
-    }
-})
+console.log(mql);
 
-document.querySelector('.input-field div').addEventListener('click', function () {
-    document.querySelector('.input-field div').style.border = 'solid 5px #808080';
-})
-
-delButton.addEventListener('click', function () {
-    let currrentInput = document.querySelector('.input-number-field').textContent;
-    if (currrentInput) {
-        let newInput = currrentInput.substring(0, currrentInput.length - 1);
-        document.querySelector('.input-number-field').textContent = newInput;
-    }
-})
-
-clearButton.addEventListener('click', function () {
-    let currrentInput = document.querySelector('.input-number-field').textContent;
-    if (currrentInput) {
-        let newInput = null;
-        document.querySelector('.input-number-field').textContent = newInput;
-    }
-})
-
-
-function display(querySelector, displaySet) {
-    if (displaySet) {
-        document.querySelector(querySelector).classList.add('show');
-    } else if (!displaySet) {
-        document.querySelector(querySelector).classList.remove('show');
-    } 
+if (mql.matches) {
+  document
+    .querySelector('.input-number-field')
+    .setAttribute('readonly', 'readonly');
 }
 
+toggleBtn.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    document.querySelector('.toggle-button').click();
+  }
+});
 
+toggleBtn.addEventListener('click', function () {
+  if (hint == true) {
+    document.querySelector('.toggle-button').classList.remove('on');
+    hint = false;
+  } else if (hint == false) {
+    document.querySelector('.toggle-button').classList.add('on');
+    hint = true;
+  }
+});
+
+keys.addEventListener('click', function (e) {
+  let clickedNumber = e.target.textContent;
+  if (!isNaN(clickedNumber)) {
+    document.querySelector('.input-number-field').value += clickedNumber;
+    document.querySelector('.input-field div').style.border =
+      'solid 5px #808080';
+  }
+});
+
+document
+  .querySelector('.input-field div')
+  .addEventListener('click', function () {
+    document.querySelector('.input-field div').style.border =
+      'solid 5px #808080';
+  });
+
+delButton.addEventListener('click', function () {
+  let currrentInput = document.querySelector('.input-number-field').textContent;
+  if (currrentInput) {
+    let newInput = currrentInput.substring(0, currrentInput.length - 1);
+    document.querySelector('.input-number-field').textContent = newInput;
+  }
+});
+
+clearButton.addEventListener('click', function () {
+  let currrentInput = document.querySelector('.input-number-field').textContent;
+  if (currrentInput) {
+    let newInput = null;
+    document.querySelector('.input-number-field').textContent = newInput;
+  }
+});
+
+function display(querySelector, displaySet) {
+  if (displaySet) {
+    document.querySelector(querySelector).classList.add('show');
+  } else if (!displaySet) {
+    document.querySelector(querySelector).classList.remove('show');
+    document.querySelector('.input-number-field').focus();
+    console.log('Hola');
+  }
+}
 
 menuButton.addEventListener('click', function () {
-    display('nav', true);
-})
+  display('nav', true);
+});
 
 for (const button of closeButton) {
-     button.addEventListener('click', function (e) {
-            this.parentNode.classList.remove('show');
-        })   
+  button.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      button.click();
+    }
+  });
+}
+
+for (const button of closeButton) {
+  button.addEventListener('click', function (e) {
+    this.parentNode.classList.remove('show');
+    document.querySelector('.input-number-field').focus();
+  });
 }
 
 rulesButton.addEventListener('click', function () {
-    display('nav', false)
-    display('.rules', true);
-})
+  display('nav', false);
+  display('.rules', true);
+});
 
 creditsButton.addEventListener('click', function () {
-    display('nav', false);
-    display('.credits', true);
-})
+  display('nav', false);
+  display('.credits', true);
+});
 
 startButton.addEventListener('click', function () {
-    welcomePage.classList.add('welcome-page-hide');
-    initiateValues();
-    document.querySelector('.hidden-number').style.fontSize = `${FONT_SIZE}px`
-    document.querySelector('.level').textContent = `${LEVEL} (1 - ${MAXIMUM_NUMBER})`;
-    document.querySelector('.current-trials-left').textContent = NUMBER_OF_TRIALS_LEFT;
-    document.querySelector('.message-log').innerHTML = message;
-})
+  welcomePage.classList.add('welcome-page-hide');
+  initiateValues();
+  document.querySelector('.hidden-number').style.fontSize = `${FONT_SIZE}px`;
+  document.querySelector(
+    '.level',
+  ).textContent = `${LEVEL} (1 - ${MAXIMUM_NUMBER})`;
+  document.querySelector('.current-trials-left').textContent =
+    NUMBER_OF_TRIALS_LEFT;
+  document.querySelector('.message-log').innerHTML = message;
+  document.querySelector('.input-number-field').focus();
+});
 
-guessButton.addEventListener('click', function () {
-    let input = document.querySelector('.input-number-field').textContent;
+for (const guess of guessButton) {
+  guess.addEventListener('click', function (e) {
+    e.preventDefault();
+    let input = document.querySelector('.input-number-field').value;
     if (input == '') {
-        message = message + 'Please enter a number'
+      message = message + 'Please enter a number';
     } else if (isNaN(input)) {
-        message = `${message} \u000A The input must be a number`
+      message = `${message} \u000A The input must be a number`;
     } else if (input > MAXIMUM_NUMBER) {
-        message = `${message} \u000A The number must not be greater than ${MAXIMUM_NUMBER}`;
+      message = `${message} \u000A The number must not be greater than ${MAXIMUM_NUMBER}`;
     } else {
-        NUMBER_OF_TRIALS_LEFT--;
-        document.querySelector('.current-trials-left').textContent = NUMBER_OF_TRIALS_LEFT;
-        if (input == SPECIAL_NUMBER) {
-            document.querySelector('.hidden-number').textContent = SPECIAL_NUMBER;
-            displaySuccess();
+      NUMBER_OF_TRIALS_LEFT--;
+      document.querySelector('.current-trials-left').textContent =
+        NUMBER_OF_TRIALS_LEFT;
+      if (input == SPECIAL_NUMBER) {
+        document.querySelector('.hidden-number').textContent = SPECIAL_NUMBER;
+        displaySuccess();
+      } else {
+        if (NUMBER_OF_TRIALS_LEFT == 0) {
+          document.querySelector('.hidden-number').textContent = SPECIAL_NUMBER;
+          displayGameFailure();
         } else {
-            if (NUMBER_OF_TRIALS_LEFT == 0) {
-                document.querySelector('.hidden-number').textContent = SPECIAL_NUMBER;
-                displayGameFailure()
+          message = `${message} <br/> Wrong, Try Again`;
+          if (hint) {
+            if (input > SPECIAL_NUMBER) {
+              message = `${message} <br/><br/> You went too <strong> high </strong>`;
             } else {
-                message = `${message} <br/> Wrong, Try Again`
-                if (hint) {
-                    if (input > SPECIAL_NUMBER) {
-                        message = `${message} <br/><br/> You went too <strong> high </strong>`;
-                    } else {
-                        message = `${message} <br/><br/> You went too <strong> low </strong>`;
-                    }
-                }
+              message = `${message} <br/><br/> You went too <strong> low </strong>`;
             }
+          }
         }
+      }
     }
 
     if (message) {
-        document.querySelector('.message-log').innerHTML = message;
-        document.querySelector('.input-field div').style.border = 'solid 5px #ff6464';
-        message = '' //clears message log
+      document.querySelector('.message-log').innerHTML = message;
+      document.querySelector('.input-field div').style.border =
+        'solid 5px #ff6464';
+      message = ''; //clears message log
     }
 
-    document.querySelector('.input-number-field').textContent = null;
-})
+    document.querySelector('.input-number-field').value = null;
+  });
+}
 
 restartButton.addEventListener('click', function () {
-    display('nav', false);
-    display('.confirmation', true);
-    action = {key:'restart', msg:'restart'}
-    document.querySelector('.action').textContent = action.msg;
-})
+  display('nav', false);
+  display('.confirmation', true);
+  action = { key: 'restart', msg: 'restart' };
+  document.querySelector('.action').textContent = action.msg;
+});
 
 selectLevel.addEventListener('click', function () {
-    display('nav', false);
-    display('.confirmation', true);
-    action = {key:'exit', msg:'exit'}
-    document.querySelector('.action').textContent = action.msg;
-})
+  display('nav', false);
+  display('.confirmation', true);
+  action = { key: 'exit', msg: 'exit' };
+  document.querySelector('.action').textContent = action.msg;
+});
 
 yesButton.addEventListener('click', function () {
-    if (action.key == 'restart') {
-        updateValues();
-        display('.confirmation', false);
-        document.querySelector('.current-trials-left').textContent = NUMBER_OF_TRIALS_LEFT;
-        document.querySelector('.message-log').innerHTML = message;
-    } else if (action.key == 'exit') {
-        deleteValues();
-        display('.confirmation', false);
-        welcomePage.classList.remove('welcome-page-hide');
-    }
-})
+  if (action.key == 'restart') {
+    updateValues();
+    display('.confirmation', false);
+    document.querySelector('.current-trials-left').textContent =
+      NUMBER_OF_TRIALS_LEFT;
+    document.querySelector('.message-log').innerHTML = message;
+  } else if (action.key == 'exit') {
+    deleteValues();
+    display('.confirmation', false);
+    welcomePage.classList.remove('welcome-page-hide');
+  }
+});
 
 playAgainButton.addEventListener('click', function () {
-    updateValues();
-    display('.game-results', false);
-    document.querySelector('.current-trials-left').textContent = NUMBER_OF_TRIALS_LEFT;
-    document.querySelector('.message-log').innerHTML = message;
-})
+  updateValues();
+  display('.game-results', false);
+  document.querySelector('.current-trials-left').textContent =
+    NUMBER_OF_TRIALS_LEFT;
+  document.querySelector('.message-log').innerHTML = message;
+});
 
 selectDifficultyButton.addEventListener('click', function () {
-     deleteValues();
-     display('.game-results', false);
-    welcomePage.classList.remove('welcome-page-hide');
-})
+  deleteValues();
+  display('.game-results', false);
+  welcomePage.classList.remove('welcome-page-hide');
+});
 
 noButton.addEventListener('click', function () {
-    display('.confirmation', false);
-})
+  display('.confirmation', false);
+});
+
+// TAB TRAPPING
 
 function getDifficulty() {
-    return parseInt(document.querySelector('.difficulty select').value);
+  return parseInt(document.querySelector('.difficulty select').value);
 }
 
 function getRandomNoAndTrials() {
-    let randomNumber
-    let trialNo
-    let max
-    let fontSize
-    switch (LEVEL) {
-        case 1:
-            randomNumber = Math.ceil(Math.random() * 10);
-            trialNo = 5;
-            max = 10;
-            fontSize = 70;
-            break;
-        
-        case 2:
-            randomNumber = Math.ceil(Math.random() * 100);
-            trialNo = 10;
-            max = 100;
-            fontSize = 50;
-            break;
-        
-        case 3:
-            randomNumber = Math.ceil(Math.random() * 1000);
-            trialNo = 15;
-            max = 1000;
-            fontSize = 35;
-        default:
-            break;
-    }
+  let randomNumber;
+  let trialNo;
+  let max;
+  let fontSize;
+  switch (LEVEL) {
+    case 1:
+      randomNumber = Math.ceil(Math.random() * 10);
+      trialNo = 5;
+      max = 10;
+      fontSize = 70;
+      break;
 
-    return [randomNumber, trialNo, max, fontSize]
+    case 2:
+      randomNumber = Math.ceil(Math.random() * 100);
+      trialNo = 10;
+      max = 100;
+      fontSize = 50;
+      break;
+
+    case 3:
+      randomNumber = Math.ceil(Math.random() * 1000);
+      trialNo = 15;
+      max = 1000;
+      fontSize = 35;
+    default:
+      break;
+  }
+
+  document.querySelector('.input-field div').style.border = 'solid 5px #808080';
+  return [randomNumber, trialNo, max, fontSize];
 }
 
-
 function initiateValues() {
-    document.querySelector('.hidden-number').textContent = '';
-    LEVEL = getDifficulty();
-    [SPECIAL_NUMBER, NUMBER_OF_TRIALS_LEFT, MAXIMUM_NUMBER, FONT_SIZE] = getRandomNoAndTrials();
-    NUMBER_OF_TRIALS = NUMBER_OF_TRIALS_LEFT;
+  document.querySelector('.hidden-number').textContent = '';
+  LEVEL = getDifficulty();
+  [SPECIAL_NUMBER, NUMBER_OF_TRIALS_LEFT, MAXIMUM_NUMBER, FONT_SIZE] =
+    getRandomNoAndTrials();
+  NUMBER_OF_TRIALS = NUMBER_OF_TRIALS_LEFT;
 }
 
 function displaySuccess() {
-    document.querySelector('.remark').textContent = 'You Win';
-    let trialsUsed = NUMBER_OF_TRIALS - NUMBER_OF_TRIALS_LEFT;
-    document.querySelector('.game-results p').innerHTML = `in ${trialsUsed} trial(s)`;
-    display('.game-results', true);
+  document.querySelector('.remark').textContent = 'You Win';
+  let trialsUsed = NUMBER_OF_TRIALS - NUMBER_OF_TRIALS_LEFT;
+  document.querySelector(
+    '.game-results p',
+  ).innerHTML = `in ${trialsUsed} trial(s)`;
+  display('.game-results', true);
 }
 
 function displayGameFailure() {
-    document.querySelector('.remark').textContent = 'Game Over';
-    document.querySelector('.game-results p').innerHTML = 'You Loose';
-    display('.game-results', true);
+  document.querySelector('.remark').textContent = 'Game Over';
+  document.querySelector('.game-results p').innerHTML = 'You Loose';
+  display('.game-results', true);
 }
 
 function updateValues() {
-    document.querySelector('.hidden-number').textContent = '';
-    SPECIAL_NUMBER = getRandomNoAndTrials()[0];
-    NUMBER_OF_TRIALS_LEFT = NUMBER_OF_TRIALS;
-    FONT_SIZE = getRandomNoAndTrials[3];
- }
+  document.querySelector('.hidden-number').textContent = '';
+  SPECIAL_NUMBER = getRandomNoAndTrials()[0];
+  NUMBER_OF_TRIALS_LEFT = NUMBER_OF_TRIALS;
+  FONT_SIZE = getRandomNoAndTrials[3];
+}
 function deleteValues() {
-    document.querySelector('.hidden-number').textContent = '';
-    SPECIAL_NUMBER = null;
-    NUMBER_OF_TRIALS = null;
-    NUMBER_OF_TRIALS_LEFT = null;
-    LEVEL = null;
-    FONT_SIZE = null;
- }
+  document.querySelector('.hidden-number').textContent = '';
+  SPECIAL_NUMBER = null;
+  NUMBER_OF_TRIALS = null;
+  NUMBER_OF_TRIALS_LEFT = null;
+  LEVEL = null;
+  FONT_SIZE = null;
+}
---
+++ serviceworker.js
@@ -0,0 +1,45 @@
+const assets = [
+  '/',
+  '/css/style.css',
+  '/js/script.js',
+  '/css/slkscr-webfont.eot',
+  '/css/slkscr-webfont.ttf',
+  '/css/slkscr-webfont.woff',
+  'sw-register.js',
+];
+
+// self.addEventListener('install', (e) => {
+//   caches.open('asstes').then((cache) => {
+//     cache.addAll(assets);
+//   });
+// });
+
+// Service Worker Installation
+self.addEventListener("install", event => {
+    // install the assets of my PWA
+    event.waitUntil(
+        caches.open("assets").then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+    // self.skipWaiting(); // activate the service worker immediately
+});
+
+
+// State while revalidate strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Even if the response is in the cache, we fetch it
+      // and update the cache for future usage
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        caches.open('assets').then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+      // We use the currently cached version if it's there
+      return cachedResponse || fetchPromise; // cached or a network fetch
+    }),
+  );
+});
